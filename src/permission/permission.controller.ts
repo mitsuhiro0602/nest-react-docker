f009695aaf4986d73add76bd1c2b7ecbeed8582d
mitsuhiro0602 <0hornet0@gmail.com>
2021-07-08 08:00:32 +0900

    permission guard

--- src/permission/permission.controller.ts
+++ src/permission/permission.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { HasPermission } from './has-permission.decorator';
 import { PermissionService } from './permission.service';
 
 @Controller('permissions')
@@ -6,6 +7,7 @@ export class PermissionController {
   constructor(private permissionService: PermissionService) {
   }
   @Get()
+  @HasPermission('view_permissions')
   async all() {
     return this.permissionService.all();
   }
